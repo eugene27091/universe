19e21a8512c60139f6e3404aba9da9f7be8c4d0c
tpghks123 <83364613+tpghks123@users.noreply.github.com>
2021-05-05 20:49:39 +0900

    메일함

---
+++ game_universe/Assets/Scripts/ClickButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickButton : MonoBehaviour
+{
+    public void SceneChange()
+    {
+        SceneManager.LoadScene("Scene1- 메일함 첫화면");
+    }
+    public void SceneChange2()
+    {
+        SceneManager.LoadScene("Scene2- 김근육");
+    }
+    public void SceneChange3()
+    {
+        SceneManager.LoadScene("Scene3- 이인싸");
+    }
+    public void SceneChange4()
+    {
+        SceneManager.LoadScene("Scene4- 박종관");
+    }
+}
