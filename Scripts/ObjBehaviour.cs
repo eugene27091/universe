3920fdb770e8c9a23d3b75929170bdff1c90ee1c
ykmoon04 <moonsc014@gmail.com>
2021-05-08 13:26:51 +0900

    집 내부 상호작용 수정

---
+++ Scripts/ObjBehaviour.cs
@@ -0,0 +1,32 @@
+// 방안에서 상호작용 가능한 각 오브젝트에 적용
+using UnityEngine;
+
+public class ObjBehaviour: MonoBehaviour
+{
+    [SerializeField] Texture2D cursorArrow;
+    [SerializeField] Texture2D cursorSearch;
+    [SerializeField] GameObject blocker;
+
+    public GameManager manager;
+    private void OnMouseDown()
+    {
+        manager.Action(GameObject.Find(this.name));
+    }
+    void OnMouseEnter()
+    {
+        Cursor.SetCursor(cursorSearch, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+    private void OnMouseExit()
+    {
+        Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+
+    private void Update()
+    {
+        if (blocker.activeSelf == true) GetComponent<BoxCollider2D>().enabled = false;
+        else GetComponent<BoxCollider2D>().enabled = true;
+    }
+
+}
